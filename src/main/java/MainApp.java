eee879d58136c118472fb9c29acc4df0ef845574
Dmytro Chebotarskyi <dm.chebotarskyi@gmail.com>
2017-11-21 23:23:49 +0100

    refactored for task 6

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,4 +1,4 @@
-import com.sun.deploy.util.StringUtils;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.RegexFileFilter;
@@ -7,21 +7,15 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.store.FSDirectory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Scanner;
 
@@ -32,11 +26,13 @@ import static java.lang.System.exit;
  */
 public class MainApp {
 
+    static private String START = "START OF THIS PROJECT GUTENBERG EBOOK";
+    static private String END = "END OF THIS PROJECT GUTENBERG EBOOK";
+
     static public void main(String[] args) throws IOException {
 
         String path = args[0];
 
-        System.out.println(path);
         File directory = new File(path);
 
         Collection<File> zips = FileUtils.listFiles(
@@ -45,6 +41,7 @@ public class MainApp {
                 DirectoryFileFilter.DIRECTORY
         );
 
+        int count = zips.size();
 
         System.out.println("Starting indexing " + zips.size());
 
@@ -53,7 +50,7 @@ public class MainApp {
         String temporaryDirectory = "/home/dima/Загрузки/PG/tmp";
 
         StandardAnalyzer analyzer = new StandardAnalyzer();
-        Directory index = new RAMDirectory();
+        FSDirectory index = FSDirectory.open(Paths.get("/home/dima/index.lucene"));
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
         IndexWriter w = new IndexWriter(index, config);
 
@@ -71,8 +68,9 @@ public class MainApp {
 
             for (File file : files) {
                 String title = findTitle(file);
+                String body = findBody(file);
                 if (title != null) {
-                    addDoc(w, title, zip.getPath());
+                    addDoc(w, title, body, zip.getPath());
                     System.out.println("Indexed: " + file.getPath());
                 }
             }
@@ -85,51 +83,15 @@ public class MainApp {
 
         w.close();
 
-        System.out.println("DONE. \n Query mode started. ");
-
-        while (true) {
-            String querystr = askForTitle();
-            if (querystr.equals("-1")) {
-                System.out.println("Bye!");
-                exit(0);
-            }
-            Query q = null;
-            try {
-                q = new QueryParser("title", analyzer).parse(querystr);
-            } catch (ParseException e) {
-                System.out.println("Error parsing query");
-            }
-
-            processQuery(index, q);
-        }
+        System.out.println("Totaly: " + count);
+        System.out.println("DONE.");
 
     }
 
-    private static String askForTitle() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\n\nPlease enter title to search: ");
-        String title = scanner.nextLine();
-        return title;
-    }
-
-    private static void processQuery(Directory index, Query q) throws IOException {
-        int hitsPerPage = 10;
-        IndexReader reader = DirectoryReader.open(index);
-        IndexSearcher searcher = new IndexSearcher(reader);
-        TopDocs docs = searcher.search(q, hitsPerPage);
-        ScoreDoc[] hits = docs.scoreDocs;
-
-        System.out.println("Found " + hits.length + " hits.");
-        for (int i = 0; i < hits.length; ++i) {
-            int docId = hits[i].doc;
-            Document d = searcher.doc(docId);
-            System.out.println((i + 1) + ". " + d.get("title") + "\t" + d.get("path"));
-        }
-    }
-
-    private static void addDoc(IndexWriter w, String title, String path) throws IOException {
+    private static void addDoc(IndexWriter w, String title, String body, String path) throws IOException {
         Document doc = new Document();
         doc.add(new TextField("title", title, Field.Store.YES));
+        doc.add(new TextField("body", body, Field.Store.YES));
         doc.add(new StringField("path", path, Field.Store.YES));
         w.addDocument(doc);
     }
@@ -153,5 +115,29 @@ public class MainApp {
             }
         }
         return null;
+    }
+
+    private static String findBody(File file) throws IOException {
+
+        InputStream stream = new FileInputStream(file);
+
+
+        final Scanner scanner = new Scanner(file);
+        StringBuilder body = new StringBuilder();
+        boolean toWrite = false;
+        while (scanner.hasNextLine()) {
+            final String lineFromFile = scanner.nextLine();
+
+            if (toWrite)
+                body.append(lineFromFile);
+                body.append("\n");
+
+            if (lineFromFile.contains(START)) {
+                toWrite = true;
+            } else if (lineFromFile.contains(END)) {
+                break;
+            }
+        }
+        return body.toString();
     }
 }
